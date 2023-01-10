14cd6c1d2d8c4f0804e8b393011ebc2de31174c7
hoonkim <hoonk9387@gmail.com>
2023-01-09 19:26:48 -0600

    Updated index action in controller to use serialization except:.

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -10,8 +10,8 @@ class CheesesController < ApplicationController
         # render json: cheeses
         
         cheeses = Cheese.all
-        render json: cheeses, only: [:id, :name, :price, :is_best_seller]
-    
+        # render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+        render json: cheeses, except: [:created_at, :updated_at]
     end
     # READ
     # GET /cheeses/:id
